4f84d40da2fbe9bdf251500f52611ff322fd5228
LuanHP <luanhenriquepinheiro15@gmail.com>
2026-03-25 20:19:38 -0300

    Abertura dos procedimentos e funções da minha parte

---
+++ Luan.pas
@@ -0,0 +1,31 @@
+Program truco;
+
+//Procedimento para ENFILEIRAR as cartas.
+procedure Enfileirar ();
+begin
+
+end;
+
+//Função para DESINFILEIRAR as cartas.
+function Desinfileirar ();
+begin
+
+end;
+
+//Procedimento para DISTRIBUIR as cartas parar cada jogador.
+procedure Distribuir();
+begin
+
+end;
+
+//Procedimento para EXIBIR MÃO: Mostra as cartas numeradas para o jogador escolher.
+procedure ExibirMao ();
+begin
+
+end;
+
+//Procedimento JOGAR CARTA: Remove a carta escolhida da lista.
+procedure JogarCarta ();
+begin
+
+end;
